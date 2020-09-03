5597eeb465275d852621c3a93af20cbf4a588b65
Christina Holt <Christina.Holt@noaa.gov>
2020-09-03 19:40:30 +0000

    Undoing a few duplicates from Chunhua's branch

--- scripts/exregional_run_fcst.sh
+++ scripts/exregional_run_fcst.sh
@@ -299,33 +299,6 @@ Cannot create symlink because target does not exist:
 fi
 
 #
-# Symlink to halo-0 orography files for GWD scheme
-#
-target="${FIXLAM}/${CRES}${DOT_OR_USCORE}oro_data_ls.tile${TILE_RGNL}.halo${NH0}.nc"
-#symlink="oro_data_ls.tile${TILE_RGNL}.halo${NH0}.nc"
-#symlink="oro_data_ls.tile${TILE_RGNL}.nc"
-symlink="oro_data_ls.nc"
-if [ -f "${target}" ]; then
-  ln_vrfy -sf ${relative_or_null} $target $symlink
-else
-  print_err_msg_exit "\
-Cannot create symlink because target does not exist:
-  target = \"$target}\""
-fi
-target="${FIXLAM}/${CRES}${DOT_OR_USCORE}oro_data_ss.tile${TILE_RGNL}.halo${NH0}.nc"
-# symlink="oro_data_ss.tile${TILE_RGNL}.halo${NH0}.nc"
-#symlink="oro_data_ss.tile${TILE_RGNL}.nc"
-symlink="oro_data_ss.nc"
-if [ -f "${target}" ]; then
-  ln_vrfy -sf ${relative_or_null} $target $symlink
-else
-  print_err_msg_exit "\
-Cannot create symlink because target does not exist:
-  target = \"$target}\""
-fi
-
-
-#
 #-----------------------------------------------------------------------
 #
 # The FV3 model looks for the following files in the INPUT subdirectory
--- ush/config_defaults.sh
+++ ush/config_defaults.sh
@@ -201,8 +201,8 @@ EXPT_SUBDIR=""
 COMINgfs="/base/path/of/directory/containing/gfs/input/files"
 STMP="/base/path/of/directory/containing/model/input/and/raw/output/files"
 NET="rrfs"
-envir="experiment_name"
-RUN="rrfs"
+envir="para"
+RUN="experiment_name"
 PTMP="/base/path/of/directory/containing/postprocessed/output/files"
 #
 #-----------------------------------------------------------------------
