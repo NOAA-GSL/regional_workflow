d35178e3277ac75e5ea0f90abdcb8ee03be3a9cb
Ming.Hu <Ming.Hu@noaa.gov>
2021-04-24 00:33:35 +0000

    Add data process for radar reflectivity, lightning, and BUFR Add analysis functions for no-var cloud analysis and radar to tten.

--- ush/generate_FV3LAM_wflow.sh
+++ ush/generate_FV3LAM_wflow.sh
@@ -193,6 +193,11 @@ settings="\
   'anal_gsi': ${ANAL_GSI_TN}
   'prep_coldstart': ${PREP_COLDSTART_TN}
   'prep_warmstart': ${PREP_WARMSTART_TN}
+  'process_radarref': ${PROCESS_RADAR_REF_TN}
+  'process_lightning': ${PROCESS_LIGHTNING_TN}
+  'process_bufr': ${PROCESS_BUFR_TN}
+  'radar_refl2tten': ${RADAR_REFL2TTEN_TN}
+  'cldanl_novar': ${CLDANL_NOVAR_TN}
   'tag': ${TAG}
 #
 # Number of nodes to use for each task.
@@ -208,6 +213,11 @@ settings="\
   'nnodes_run_fcst': ${NNODES_RUN_FCST}
   'nnodes_run_anal': ${NNODES_RUN_ANAL}
   'nnodes_run_post': ${NNODES_RUN_POST}
+  'nnodes_proc_radar': ${NNODES_PROC_RADAR}
+  'nnodes_proc_lightning': ${NNODES_PROC_LIGHTNING}
+  'nnodes_proc_bufr': ${NNODES_PROC_BUFR}
+  'nnodes_run_ref2tten': ${NNODES_RUN_REF2TTEN}
+  'nnodes_run_novarcldanl': ${NNODES_RUN_NOVARCLDANL}
 #
 # Number of cores used for a task
 #
@@ -230,6 +240,11 @@ settings="\
   'ppn_run_fcst': ${PPN_RUN_FCST}
   'ppn_run_anal': ${PPN_RUN_ANAL}
   'ppn_run_post': ${PPN_RUN_POST}
+  'ppn_proc_radar': ${PPN_PROC_RADAR}
+  'ppn_proc_lightning': ${PPN_PROC_LIGHTNING}
+  'ppn_proc_bufr': ${PPN_PROC_BUFR}
+  'ppn_run_ref2tten': ${PPN_RUN_REF2TTEN}
+  'ppn_run_novarcldanl': ${PPN_RUN_NOVARCLDANL}
 #
 # Maximum wallclock time for each task.
 #
@@ -244,6 +259,16 @@ settings="\
   'wtime_run_fcst': ${WTIME_RUN_FCST}
   'wtime_run_anal': ${WTIME_RUN_ANAL}
   'wtime_run_post': ${WTIME_RUN_POST}
+  'wtime_proc_radar': ${WTIME_PROC_RADAR}
+  'wtime_proc_lightning': ${WTIME_PROC_LIGHTNING}
+  'wtime_proc_bufr': ${WTIME_PROC_BUFR}
+  'wtime_run_ref2tten': ${WTIME_RUN_REF2TTEN}
+  'wtime_run_novarcldanl': ${WTIME_RUN_NOVARCLDANL}
+#
+# Maximum memory for each task.
+#
+  'memo_run_ref2tten': ${MEMO_RUN_REF2TTEN}
+  'memo_run_novarcldanl': ${MEMO_RUN_NOVARCLDANL}
 #
 # Maximum number of tries for each task.
 #
@@ -258,6 +283,11 @@ settings="\
   'maxtries_run_fcst': ${MAXTRIES_RUN_FCST}
   'maxtries_anal_gsi': ${MAXTRIES_ANAL_GSI}
   'maxtries_run_post': ${MAXTRIES_RUN_POST}
+  'maxtries_process_radarref': ${MAXTRIES_PROCESS_RADARREF}
+  'maxtries_process_lightning': ${MAXTRIES_PROCESS_LIGHTNING}
+  'maxtries_process_bufr': ${MAXTRIES_PROCESS_BUFR}
+  'maxtries_radar_ref2tten': ${MAXTRIES_RADAR_REF2TTEN}
+  'maxtries_cldanl_novar': ${MAXTRIES_CLDANL_NOVAR}
 #
 # Flags that specify whether to run the preprocessing tasks.
 #
