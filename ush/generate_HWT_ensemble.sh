706d10cf264d5b95c4af9d7fff59302bdcdb8706
Christina Holt <christina.holt@noaa.gov>
2021-06-15 19:17:47 +0000

    Turn off debug set -x

--- ush/generate_HWT_ensemble.sh
+++ ush/generate_HWT_ensemble.sh
@@ -3,10 +3,7 @@
 # Remove config.sh in case it's a link
 rm -rf config.sh
 
-set -x
-
 # Generate 9 individual member experiment directories following the same numbering as in HWT.
-
 mem=1
 for suite in FV3_GFS_v15_thompson_mynn rrfs_gfsv16 nssl_mp_no_nsst ; do 
    for ICS in gfs gefs01 gefs02; do 
